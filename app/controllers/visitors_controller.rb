2dcf892959c9178db104f1a042da55a5a189190c
Anthony Carter <anthonycarter@mt4sol.com>
2015-08-19 20:15:13 -0400

    2nd commit

---
+++ app/controllers/visitors_controller.rb
@@ -0,0 +1,7 @@
+# visitors_controller.rb
+
+class VisitorsController < ApplicationController
+	def new
+		@owner = Owner.new
+	end
+end
---
+++ app/models/owner.rb
@@ -0,0 +1,24 @@
+# owner.rb
+
+class Owner
+
+	def name
+		name = 'Anthony'
+	end
+
+	def birthdate
+		birthdate = Date.new(1971, 9, 30)
+	end
+	
+	def countdown
+		today = Date.today
+
+		birthday = Date.new(today.year, birthdate.month, birthdate.day)
+
+		if birthday > today
+			countdown = (birthday - today).to_i
+		else
+			countdown = (birthday.next_year - today).to_i
+		end
+	end
+end
